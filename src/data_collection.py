f9e9bdeb6b40093355289af12ad365336ea1a0fc
Chukwuemeka <chukwuemekaeze270@gmail.com>
2025-03-20 10:37:49 +0100

    data source update

--- src/data_collection.py
+++ src/data_collection.py
@@ -4,3 +4,4 @@ data_collection.py
 This module will contain functions and/or classes for collecting data,
 either via web scraping or API calls.
 """
+ #We will scrape a news website,” or “We will use Twitter’s API.”
